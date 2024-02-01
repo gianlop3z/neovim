be1c753d5791e407b337a84ace0ede19405dc55c
Gian López <lopezarizagianlucas@gmail.com>
2024-02-01 14:29:53 -0500

    [breakpoint]: migrado todo a `LazyVim`

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        "lua",
+        "python",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "scss",
+        "dart",
+      },
+    },
+  },
+}
