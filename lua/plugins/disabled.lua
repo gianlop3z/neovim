055f6bce3d1edb424f8c29369a5b06b15869b27a
Gian López <lopezarizagianlucas@gmail.com>
2024-02-01 15:27:55 -0500

    [refactor]: eliminados algunos plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,10 +1,22 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-  },
+	{
+		"akinsho/bufferline.nvim",
+		enabled = false,
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		enabled = false,
+	},
+	{
+		"echasnovski/mini.bufremove",
+		enabled = false,
+	},
+	{
+		"folke/trouble.nvim",
+		enabled = false,
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		enabled = false,
+	},
 }
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,87 +1,95 @@
 return {
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
-    keys = {
-      {
-        "<leader>tg",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.live_grep()
-        end,
-        desc = "Open `telescope` live grep picker",
-      },
-      {
-        "<leader>tf",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.find_files({ hidden = true })
-        end,
-        desc = "Open `telescope` file finder picker",
-      },
-      {
-        "<leader>to",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.buffers()
-        end,
-        desc = "Open `telescope` open buffers picker",
-      },
-      {
-        "<leader>tb",
-        function()
-          local telescope = require("telescope")
-          telescope.extensions.file_browser.file_browser()
-        end,
-        desc = "",
-      },
-      {
-        "<leader>tr",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.resume()
-        end,
-        desc = "Open `telescope` resume picker",
-      },
-      {
-        "<leader>td",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.diagnostics()
-        end,
-        desc = "Open `telescope` diagnostics picker",
-      },
-      {
-        "<leader>tt",
-        function()
-          local builtin = require("telescope.builtin")
-          builtin.treesitter()
-        end,
-        desc = "Open `telescope` treesitter picker",
-      },
-    },
-    config = function(_, opts)
-      local telescope = require("telescope")
-      local fb_actions = require("telescope").extensions.file_browser.actions
+	{
+		"telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		keys = function()
+			return {
+				{
+					"<leader>tg",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.live_grep()
+					end,
+					desc = "Open `telescope` live grep picker",
+				},
+				{
+					"<leader>tf",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.find_files({ hidden = true })
+					end,
+					desc = "Open `telescope` file finder picker",
+				},
+				{
+					"<leader>to",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.buffers()
+					end,
+					desc = "Open `telescope` open buffers picker",
+				},
+				{
+					"<leader>tb",
+					function()
+						local telescope = require("telescope")
+						telescope.extensions.file_browser.file_browser()
+					end,
+					desc = "",
+				},
+				{
+					"<leader>tr",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.resume()
+					end,
+					desc = "Open `telescope` resume picker",
+				},
+				{
+					"<leader>td",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.diagnostics()
+					end,
+					desc = "Open `telescope` diagnostics picker",
+				},
+				{
+					"<leader>tt",
+					function()
+						local builtin = require("telescope.builtin")
+						builtin.treesitter()
+					end,
+					desc = "Open `telescope` treesitter picker",
+				},
+			}
+		end,
+		config = function(_, opts)
+			local telescope = require("telescope")
 
-      opts.extensions = {
-        file_browser = {
-          path = "%:p:h",
-          hide_parent_dir = true,
-          git_status = false,
-          grouped = true,
-          hidden = true,
-          sorting_strategy = "ascending",
-          initial_mode = "normal",
-          previewer = false,
-        },
-      }
+			opts.defaults = {
+				file_ignore_patterns = {
+					-- git
+					".git/",
+				},
+			}
 
-      telescope.setup(opts)
-      telescope.load_extension("file_browser")
-      telescope.load_extension("fzf")
-    end,
-  },
+			opts.extensions = {
+				file_browser = {
+					path = "%:p:h",
+					hide_parent_dir = true,
+					git_status = false,
+					grouped = true,
+					hidden = true,
+					sorting_strategy = "ascending",
+					initial_mode = "normal",
+					previewer = false,
+				},
+			}
+
+			telescope.setup(opts)
+			telescope.load_extension("file_browser")
+			telescope.load_extension("fzf")
+		end,
+	},
 }
