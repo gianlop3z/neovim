974e4112376bc58ab71cdf662bec750122795288
Gian López <lopezarizagianlucas@gmail.com>
2024-04-22 22:35:49 -0500

    feat(plugins): agregado `harpoon`.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
+		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- { import = "lazyvim.plugins.extras.ui.alpha" },
