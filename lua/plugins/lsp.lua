2cd7e36cebe0bb0dc31070d51b0e55c997f0998f
Gian López <lopezarizagianlucas@gmail.com>
2024-06-21 15:37:10 -0500

    refactor(lsp): `inlay_hints` disabled.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,20 @@
 return {
 	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = {
+				enabled = false,
+			},
+		},
+	},
+	{
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
 			vim.list_extend(opts.ensure_installed, {
 				-- lua
 				"lua-language-server",
+				-- python
+				"ruff",
 			})
 		end,
 	},
